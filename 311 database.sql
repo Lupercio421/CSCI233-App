b497a8400a01af77138815d06186fdf50b2737c8
Daniel Lupercio <daniel.llupercio@gmail.com>
2020-09-18 00:24:21 -0400

    little bit of the data

--- 311 database.sql
+++ 311 database.sql
@@ -1,3 +1,18 @@
-CREATE DATABASE IF NOT EXISTS 311calls;
+
 USE 311calls;
 SHOW TABLES;
+create table complaints (
+id INT PRIMARY KEY,
+created_date TIMESTAMP,
+closed_date TIMESTAMP,
+agency_name CHAR(70),
+incident_zip INT,
+indident_address VARCHAR(70),
+borough CHAR(12),
+lattitude FLOAT(9,7),
+longitude FLOAT(9,7),
+location VARCHAR(255) )
+
+
+
+
